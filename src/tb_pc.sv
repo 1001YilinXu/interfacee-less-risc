231ab3b892c400781d9a00c351a59fe43e824cb3
Emri <eeshtref@purdue.edu>
2024-06-25 14:34:57 +0000

    updated make fil

--- src/tb_pc.sv
+++ src/tb_pc.sv
@@ -1,12 +1,16 @@
-`include "src/pc.sv"
 
 `timescale 1ms / 100us
 
+typedef enum logic [5:0] {
+		CU_LUI, CU_AUIPC, CU_JAL, CU_JALR, 
+		CU_BEQ, CU_BNE, CU_BLT, CU_BGE, CU_BLTU, CU_BGEU, 
+		CU_LB, CU_LH, CU_LW, CU_LBU, CU_LHU, CU_SB, CU_SH, CU_SW, 
+		CU_ADDI, CU_SLTI, CU_SLTIU, CU_SLIU, CU_XORI, CU_ORI, CU_ANDI, CU_SLLI, CU_SRLI, CU_SRAI, 
+		CU_ADD, CU_SUB, CU_SLL, CU_SLT, CU_SLTU, CU_XOR, CU_SRL, CU_SRA, CU_OR, CU_AND,
+		CU_ERROR
+	} cuOPType;	
 
 module tb_PCaddr();
-
-pc DUT(.cuOP(tb_cuOP), .rs1Read(tb_rs1Read), .signExtend(tb_signExtend), .ALUneg(tb_ALUneg), .Zero(tb_Zero),
-.iready(tb_iready), .clk(tb_clk), .nRST(tb_nRST), .PCaddr(tb_PCaddr));
 
 localparam CLK_PERIOD = 10; 
 
@@ -17,6 +21,9 @@ logic tb_iready, tb_nRST, tb_ALUneg, tb_Zero, tb_checking_outputs;
 logic [31:0] tb_rs1Read, tb_signExtend, tb_PCaddr, tb_intermResult;
 integer tb_numOfTests, tb_test_num;
 string tb_test_case;
+
+pc DUT(.cuOP(tb_cuOP), .rs1Read(tb_rs1Read), .signExtend(tb_signExtend), .ALUneg(tb_ALUneg), .Zero(tb_Zero),
+.iready(tb_iready), .clk(tb_clk), .nRST(tb_nRST), .PCaddr(tb_PCaddr));
 
 
 // Clock generation block
