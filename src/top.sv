78046e50e22218f987af9033fafd17686e882afd
Yilin Xu <xu1656@ee65lnx38.ecn.purdue.edu>
2024-06-26 09:25:10 -0400

    top

--- src/top.sv
+++ src/top.sv
@@ -16,13 +16,17 @@ module top (
   output logic txclk, rxclk,
   input  logic txready, rxready
 );
-logic [31:0] instruction, muxOut, aluIn, aluOut, immOut, pc, memload, writeData;
+logic [31:0] instruction, muxOut, aluIn, aluOut, immOut, pc, memload, writeData, regData1, regData2;
 logic [5:0] cuOP;
-logic [4:0] reg_1, reg_2, w_reg;
+logic [4:0] regsel1, regsel2, w_reg;
 logic [3:0] aluOP;
 logic [19:0] imm;
 
 logic clk, nrst, zero, negative, regWrite, aluSrc, i_ready, d_ready;
+
+assign nrst = pb[19];
+assign clk = hz100;
+assign left[4:0] = reg
 
 mux aluMux(.in1(reg_2), .in2(immOut), .en(aluSrc), .out(aluIn));
 
@@ -40,17 +44,13 @@ control controller (.cuOP(tb_cuOP), .instruction(tb_instructions),
 pc testpc(.clk(clk), .nRST(nrst), .ALUneg(negative), .Zero(zero), .iready(i_ready), .PCaddr(pc), .cuOP(cuOP), .rs1Read(reg_1), .signExtend(immOut));
 
 writetoReg write(.cuOP(cuOP), .memload(memload), .aluOut(aluOut), .immOut(immOut), .pc(pc), .writeData(writeData));
-endmodule
 
-
-module mux(
-	input logic in1, in2, en,
-	output logic out);
-
-	always_comb begin
-		if(en) 
-			out = in1;
-		else
-			out = in2;
-	end
+// ssdec ss1();
+// ssdec ss2();
+// ssdec ss3();
+// ssdec ss4();
+// ssdec ss5();
+// ssdec ss6();
+// ssdec ss7();
+// ssdec ss8();
 endmodule
